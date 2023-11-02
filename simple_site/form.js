d26dab7eba556f96b84cc43db6dcd6bb1dab1c81
ivan <bogatyryovvanya@yandex.ru>
2023-11-02 14:31:22 +0300

    План выполнен

--- simple_site/form.js
+++ simple_site/form.js
@@ -1,12 +1,32 @@
 const popUp = document.getElementById("popUp");
+
 popUp.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(popUp);
 
-  const one = formData.get("one");
-  const two = formData.get("two");
-  const three = formData.get("three");
-  const four = formData.get("four");
-  const five = formData.get("five");
-  console.log(one, two, five);
+  const firstName = formData.get("firstName");
+  const secondName = formData.get("secondName");
+  const lastName = formData.get("lastName");
+  const colour = formData.get("colour");
+  const pet = formData.get("pet");
+
+  let data = {
+    fam: firstName,
+    name: secondName,
+    fat: lastName,
+    col: colour,
+    pt: pet,
+  };
+
+  var str = JSON.stringify(data, null, 2);
+
+  let pre = document.createElement("pre");
+
+  pre.innerText = str;
+
+  document.body.appendChild(pre);
+
+  popupBg.classList.remove("active");
+  popUp.classList.remove("active");
+  e.target.reset();
 });
